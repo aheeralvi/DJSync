df3a3a7f2b8dbcdadb4c1fdb2c3f89150fee39ed
Zain Ahmed <zainahmed1559@gmail.com>
2017-06-19 15:45:05 -0400

    Videos now play from a function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//
+function startVideo(Videoid, startTime) {
+    var videoLink = ("http://www.youtube.com/embed/" + Videoid + "?start=" + startTime + "&autoplay=1&controls=0&showinfo=1&disablekb=1")
+        // alert(videoLink);
+    document.getElementById('player').src = videoLink;
+    alert('hi');
+    return;
+}
+
+startVideo("vW4tyQ5XD_Q", "60");
