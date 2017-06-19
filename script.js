b03048be5def04d800fc4ee49808be821869ab72
Aheer Alvi <aheeralvi@Aheers-MacBook-Pro.local>
2017-06-19 15:47:08 -0400

    Deleted Alert

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function startVideo(Videoid, startTime) {
     var videoLink = ("http://www.youtube.com/embed/" + Videoid + "?start=" + startTime + "&autoplay=1&controls=0&showinfo=1&disablekb=1")
         // alert(videoLink);
     document.getElementById('player').src = videoLink;
-    alert('hi');
     return;
 }
 
