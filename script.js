70a6219a3613475ba35fc1626d610f0884f6f314
Zain Ahmed <zainahmed1559@gmail.com>
2017-06-20 11:24:44 -0400

    added broken firebase

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function startVideo(Videoid, startTime) {
 }
 
 startVideo("vW4tyQ5XD_Q", "60");
+
+
+function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
+        // [START createprovider]
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        // [END addscopes]
+        // [START signin]
+        firebase.auth().signInWithRedirect(provider);
+        // [END signin]
+    } else {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+    }
+    // [START_EXCLUDE]
+    document.getElementById('quickstart-sign-in').disabled = true;
+    // [END_EXCLUDE]
+}
