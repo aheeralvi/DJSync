9ddc7c354036f361e965eb130ffdf5fad9fe31b0
Aheer Alvi <aheeralvi@Aheers-MacBook-Pro.local>
2017-06-20 13:52:26 -0400

    Add separate login screen, login script, added bg image

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAH7CQ-JuB5KFJqFGrQXIF_NxMp-glz6PY",
+    authDomain: "music-sync-8212d.firebaseapp.com",
+    databaseURL: "https://music-sync-8212d.firebaseio.com",
+    projectId: "music-sync-8212d",
+    storageBucket: "music-sync-8212d.appspot.com",
+    messagingSenderId: "317967865570"
+};
+firebase.initializeApp(config);
+
+
 function startVideo(Videoid, startTime) {
     var currentTime = (new Date).toTimeString().slice(0, 8);
     var videoLink = ("http://www.youtube.com/embed/" + Videoid + "?start=" + startTime + "&autoplay=1&controls=0&showinfo=1&disablekb=1")
@@ -13,50 +25,3 @@ function startVideo(Videoid, startTime) {
 }
 
 startVideo("vW4tyQ5XD_Q", "60");
-
-
-// sign in stuff
-const txtemail = document.getElementById('txtemail');
-const txtpassword = document.getElementById('txtpassword');
-const signup = document.getElementById('signup');
-const signin = document.getElementById('signin');
-const logout = document.getElementById('logout');
-
-logout.addEventListener('click', e => {
-    firebase.auth().signOut();
-
-});
-//add sign in event
-signin.addEventListener('click', e => {
-    //get email and password
-    const email = txtemail.value;
-    const password = txtpassword.value;
-    const auth = firebase.auth();
-    //sign in
-    const promise = auth.signInWithEmailAndPassword(email, password);
-
-    promise.catch(e => console.log(e.message));
-});
-
-//add sign up event
-signup.addEventListener('click', e => {
-    //get email and password
-    const email = txtemail.value;
-    const password = txtpassword.value;
-    const auth = firebase.auth();
-    const promise = auth.createUserWithEmailAndPassword(email, password);
-    promise.catch(e => console.log(e.message));
-});
-
-//add realtime authentication listener
-firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-        console.log(firebaseUser);
-        logout.style.display = 'inline-block'
-        var user = firebase.auth().currentUser;
-        alert(user.uid);
-    } else {
-        console.log('not logged in');
-        logout.style.display = 'none'
-    }
-});
